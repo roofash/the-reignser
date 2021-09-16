a0785707b6a5abfbd2a77e46e36dbf00952f5d31
roofash <90003109+roofash@users.noreply.github.com>
2021-09-16 14:30:54 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION" ]});
+
+const memberCounter = require('./counters/member-counter');
+
+client.once('ready' , () => {
+    console.log('the reignsiner is online!');
+    client.user.setActivity('dante is a boign');
+    memberCounter(client);
+});
+
+client.on('guildMemberAdd', guildMember =>{
+    let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'member');
+    guildMember.roles.add(welcomeRole);
+    guildMember.guild.channels.cache.get('865699105480638504').send(`Welcum <@${guildMember.user.id}> to our booty serbr`)
+    });
+    
+
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+
+['command_handler', 'event_handler'].forEach(handler =>{
+    require(`./handlers/${handler}`)(client, Discord);
+
+})
+
+
+
+
+client.login("ODY1Mzk5NjE5MTk4NjQ4MzMw.YPDcMw.GfJARbjSOiB1a7fmVQJ6DVI2Tlc");
